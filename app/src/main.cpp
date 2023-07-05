3302f9294c1c0ab9f85ce19866bfd67517bdde31
Maksim Drachov <maksim.drachov@outlook.com>
2023-07-05 15:33:17 +0300

    main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,8 +3,8 @@
 
 int main()
 {
-    int x;
-    std::cout << "The value of x is: " << x << std::endl;
+    // int x;
+    // std::cout << "The value of x is: " << x << std::endl;
 
     while (true)
     {
